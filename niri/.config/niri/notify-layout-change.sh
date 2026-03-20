6537ced343b7211c24bf8f63b5efe4d3f0190846
trobukan <trobukan@gmail.com>
2026-03-20 12:49:21 -0300

    [feat] Update waybar

--- niri/.config/niri/notify-layout-change.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-niri msg -j event-stream | while read -r line; do
-  is_layout_event=$(echo "$line" | jq 'has("KeyboardLayoutSwitched")')
-  if $is_layout_event; then
-    layout=$(niri msg -j keyboard-layouts | jq -r '.names[.current_idx]')
-    notify-send "Layout Changed: $layout"
-  fi
-done
