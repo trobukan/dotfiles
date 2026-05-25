7ef8251843f0230e2675b6565dec9cd633674fbc
trobukan <trobukan@gmail.com>
2026-05-24 21:45:25 -0300

    [feat] Restructure nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,8 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- load configs
+require("config.options")
+require("config.theme")
+require("config.keymaps")
+require("config.autocmds")
+
+-- load plugins
+require("plugins")
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("n", "<Esc>", function()
+	vim.cmd("noh")
+end)
+
+vim.keymap.set({ "n" }, "<C-h>", "<C-w>h")
+vim.keymap.set({ "n" }, "<C-j>", "<C-w>j")
+vim.keymap.set({ "n" }, "<C-k>", "<C-w>k")
+vim.keymap.set({ "n" }, "<C-l>", "<C-w>l")
+
+vim.keymap.set("n", "<leader>e", function()
+	vim.cmd("lua MiniFiles.open()")
+end)
